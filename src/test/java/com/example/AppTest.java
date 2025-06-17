a0162d8ae6ff6ec257629e9f622317d4994136cc
1Bartek2 <bartek.gorzynski7@gmail.com>
2025-06-17 14:23:17 +0200

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,18 +1,10 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
     @Test
-    public void testApp() {
-        assertTrue(true);
-    } 
-public class TestError {
-    public static void main(String[] args) {
-        int a = 10 / 0;  // <-- celowe dzielenie przez zero
+    public void testDoesNothing() {
+        // This test does nothing, Sonar will report it as a code smell
     }
-}
-
-    
 }
