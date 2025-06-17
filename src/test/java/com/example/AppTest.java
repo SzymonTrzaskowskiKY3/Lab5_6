8664ebca21a91b903e21d6f3fca6cc9498dfa4b8
1Bartek2 <bartek.gorzynski7@gmail.com>
2025-06-17 14:32:13 +0200

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,18 +1,8 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class AppTest {
-    @Test
-    public void testApp() {
-        assertTrue(true);
-    } 
-public class TestError {
-    public static void main(String[] args) {
-        int a = 10 / 0;  // <-- celowe dzielenie przez zero
+public class Danger {
+    public void riskyMethod() {
+        String s = null;
+        s.length(); // ❌ Null dereference — SonarCloud uzna to za Blocker Bug
     }
-}
-
-    
 }
