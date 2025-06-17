1591de3a45487c8f47511c69d8ac0c13bce77d42
1Bartek2 <bartek.gorzynski7@gmail.com>
2025-06-17 14:26:51 +0200

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,18 +1,11 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
-    @Test
-    public void testApp() {
-        assertTrue(true);
-    } 
-public class TestError {
-    public static void main(String[] args) {
-        int a = 10 / 0;  // <-- celowe dzielenie przez zero
-    }
-}
 
-    
+    @Test
+    public void testBadPractice() {
+        System.out.println("Debug info"); // ❌ Sonar zgłasza "Avoid using System.out"
+    }
 }
