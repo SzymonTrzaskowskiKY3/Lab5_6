2c602f405927b8ee3508d154202999a1fc6e981a
1Bartek2 <bartek.gorzynski7@gmail.com>
2025-06-17 14:28:36 +0200

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,18 +1,18 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
-    @Test
-    public void testApp() {
-        assertTrue(true);
-    } 
-public class TestError {
-    public static void main(String[] args) {
-        int a = 10 / 0;  // <-- celowe dzielenie przez zero
-    }
-}
 
-    
+    @Test
+    public void test1() {
+        String msg = "duplicate!";
+        System.out.println(msg);
+    }
+
+    @Test
+    public void test2() {
+        String msg = "duplicate!";
+        System.out.println(msg);
+    }
 }
