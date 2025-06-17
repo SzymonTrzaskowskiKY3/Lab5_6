0cc8ba41bd484105b41c77cd427cf4c21041c425
SzymonTrzaskowskiKY3 <u56688@student.wat.edu.pl>
2025-06-17 14:45:55 +0200

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -3,7 +3,7 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class AppTest {
+public class AppTest { 
     @Test
      public static void main(String[] args) {
         String x = null;
