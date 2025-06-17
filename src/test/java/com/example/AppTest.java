6dbdc2a77e1f24e56619f069230157d9da8eec8b
SzymonTrzaskowskiKY3 <u56688@student.wat.edu.pl>
2025-06-17 14:08:48 +0200

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -6,6 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class AppTest {
     @Test
     public void testApp() {
-        assertTrue(true);
+        assertTrue(1+1 ==3);
     } 
 }
